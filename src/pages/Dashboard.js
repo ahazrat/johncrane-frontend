3df0af7ee04a2a332c78cec9573293616689606a
Asif Hazrat <asifhazrat@gmail.com>
2018-10-06 16:10:03 -0500

    add search controls

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Input from '@material-ui/core/Input'
+import Button from '@material-ui/core/Button'
 
-const Dashboard = () => {
-    return (
-        <div>Jello world</div>
-    )
+class Dashboard extends React.Component {
+    state = {
+        searchText: 'Search Text'
+    }
+    handleSubmit(e) {
+        const searchText = document.getElementById('searchText').value
+        console.log(searchText)
+    }
+    render() {
+        return (
+            <div>
+                <br /><br />
+                <Input id='searchText' onChange={e=>this.setState({ searchText: e.target.value })} /><br /><br />
+                {this.state.searchText}<br /><br />
+                <Button variant='contained' onClick={this.handleSubmit}>Submit</Button>
+            </div>
+
+        )
+    }
 }
 
 export default Dashboard
