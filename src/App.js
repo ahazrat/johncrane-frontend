2f6ee77471fb7a754a40221055369ee3de126c1b
Asif Hazrat <asifhazrat@gmail.com>
2018-10-06 13:49:27 -0500

    install router, add pages

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
-import React, { Component } from 'react';
-import logo from './svg/logo.svg';
-import './css/logo-animation.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 
 const style = {
   app: {
     textAlign: 'center'
-  },
-  header: {
-    backgroundColor: '#282c34',
-    minHeight: '50vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 'calc(10px + 2vmin)',
-    color: 'white',
-  },
-  logo: {
-    animation: 'App-logo-spin infinite 20s linear',
-    height: '10vmin',
-  },
-  link: {
-    color: '#61dafb'
   }
 }
 
 class App extends Component {
   render() {
     return (
-      <div style={style.app}>
-        <header style={style.header}>
-          <h1>John Crane</h1>
-          <img src={logo} style={style.logo} alt="logo" />
-          <p>Interact with the <code>CARLOG</code> database</p>
-          <a href="#dashboard" style={style.link}>Get Started</a>
-        </header>
-      </div>
-    );
+      <BrowserRouter>
+        <div style={style.app}>
+          <Route path='/home' component={Home} />
+          <Route path='/dashboard' component={Dashboard} />
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import logo from '../svg/logo.svg'
+import '../css/logo-animation.css'
+
+const style = {
+    header: {
+        backgroundColor: '#282c34',
+        minHeight: '50vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 'calc(10px + 2vmin)',
+        color: 'white',
+    },
+    logo: {
+        animation: 'App-logo-spin infinite 20s linear',
+        height: '10vmin',
+    },
+    link: {
+        color: '#61dafb'
+    }
+}
+
+class Home extends Component {
+    render() {
+        return (
+            <header style={style.header}>
+                <h1>John Crane</h1>
+                <img src={logo} style={style.logo} alt="logo" />
+                <p>Interact with the <code>CARLOG</code> database</p>
+                <a href="#dashboard" style={style.link}>Get Started</a>
+            </header>
+        )
+    }
+}
+
+export default Home
